ff30d95ded7794558972c97f89e92ce5bea5ea68
norizara <narizura88@gmail.com>
2025-02-06 13:37:36 +0700

    -

--- Java OOP/Library/src/library/Main.java
+++ Java OOP/Library/src/library/Main.java
@@ -3,9 +3,59 @@ import java.util.*;
 
 public class Main {
 
-	public Main()
-	{
-	Author author1 = new Author("James Gosling", "Creator of the Java programming language.");
+    public class Author{
+        private String nama;
+        private String autobio;
+        
+        public Author(String nama, String autobio){
+            this.nama = nama;
+            this.autobio = autobio;
+        }
+        
+        public String getName(){
+            return nama;
+        }
+        public String getAuto(){
+            return autobio;
+        }
+    }
+    
+    public class Book{
+        private String nama;
+        private String ISBN;
+        private Author author;
+        
+        public Book(String nama, String ISBN, Author author){
+            this.nama = nama;
+            this.ISBN = ISBN;
+            this.author = author;
+        }
+        
+        public String getName(){
+            return nama;
+        }
+        public String getISBN(){
+            return ISBN;
+        }
+        public Author getAuthor(){
+            return author;
+        }
+    }
+    
+    public class Library{
+        ArrayList<Book> books;
+        public Library(){
+        books = new ArrayList<>();        
+    }
+        
+        public void addBook(Book book){
+            books.add(book);
+        }
+    }
+    
+    public Main()
+    {
+        Author author1 = new Author("James Gosling", "Creator of the Java programming language.");
         Author author2 = new Author("Robert C. Martin", "Known for his works on software craftsmanship.");
 
         Book book1 = new Book("Java Programming", "123456789", author1);
@@ -24,12 +74,14 @@ public class Main {
         borrower.displayBorrowedBooks();
 
         library.returnBook(borrower, book1);
-
+    
         borrower.displayBorrowedBooks();
         
         library.displayLibraryBooks();
-		}
-		
+}
+    
+    
+
     public static void main(String[] args) {
 	new Main();
     }
