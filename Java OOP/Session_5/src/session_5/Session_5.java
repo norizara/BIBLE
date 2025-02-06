45070c8cb867b8c0d644a1f1b4a72958a38842b6
norizara <narizura88@gmail.com>
2025-02-06 10:23:52 +0700

    -

--- Java OOP/Session_5/src/session_5/Session_5.java
+++ Java OOP/Session_5/src/session_5/Session_5.java
@@ -14,7 +14,7 @@ public class Session_5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
