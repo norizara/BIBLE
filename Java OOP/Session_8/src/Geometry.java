5d7e2f762c26e118cb95f824e48c5c69ac015672
norizara <narizura88@gmail.com>
2025-04-14 09:08:18 +0700

    -

---
+++ Java OOP/Session_8/src/Geometry.java
@@ -0,0 +1,13 @@
+
+public class Geometry {
+	float area;
+	protected float circumference;
+	
+	public Geometry() {
+		System.out.println("geometry constructor called");
+	}
+	
+	public void RecalculateArea() {
+		
+	}
+}
