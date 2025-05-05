4ea7d222d17d4d8f71621164631dcf2d2ab4a817
norizara <narizura88@gmail.com>
2025-05-05 08:44:34 +0700

    Project Reupload

--- HCI Project/palorant/main.js
+++ HCI Project/palorant/main.js
@@ -1,4 +1,40 @@
+function menubar() {
+    document.querySelector(".menu-data").classList.add("active");
+    document.getElementById("Menu-bar").style.display = "none";
+    document.getElementById("close-icon").style.display = "block";
+}
+
+function closebar() {
+    document.querySelector(".menu-data").classList.remove("active");
+    document.getElementById("Menu-bar").style.display = "block";
+    document.getElementById("close-icon").style.display = "none";
+}
+
+window.onclick = function(event) {
+    const menu = document.querySelector(".menu-data");
+    const menuIcon = document.getElementById("Menu-bar");
+
+    if (event.target !== menu && event.target !== menuIcon && menu.classList.contains("active")) {
+        closebar();
+    }
+};
+
+window.addEventListener("resize", function () {
+    if(window.innerWidth > 768){
+        document.querySelector(".menu-data").classList.remove("active");
+        document.getElementById("Menu-bar").style.display = "none";
+        document.getElementById("close-icon").style.display = "none";
+    }else{
+        document.getElementById("Menu-bar").style.display = "block";
+    }
+});
+
 function clickAgent()
 {
     window.location.href="agent.html";
+    }
+
+function clickMaps()
+{
+    window.location.href="maps.html";
     }
